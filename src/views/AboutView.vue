3a8311e24b8718c61350b5764aa23a43b52d3fac
Resvil <kolyha5@yandex.ru>
2022-08-26 21:55:08 +0300

    Initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1 class="text-3xl font-bold underline">This is an about page</h1>
+    <p>Test text</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<!--style lang="scss">
+.about {
+  //@apply lg:min-h-screen lg:flex lg:items-center;
+  //h1 {
+  //  @apply text-xl font-medium;
+  //}
+}
+</style-->
